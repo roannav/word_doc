8cb2dad87d94f8a2c3f6b8ff165b26094b8d0885
Roanna Victorio <roannav@users.noreply.github.com>
2022-02-18 13:21:22 -0800

    add option for double spacing between paragraphs

--- get_text_from_word_doc.py
+++ get_text_from_word_doc.py
@@ -2,17 +2,23 @@
 
 import docx
 
-def get_text_from_word_doc( filename):
+def get_text_from_word_doc( filename, double_space_between_paragraph=True):
     doc = docx.Document(filename)
     text = []
     for paragraph in doc.paragraphs:
         text.append(paragraph.text)
-    return '\n'.join(text)
+    paragraph_separator = '\n'
+    if double_space_between_paragraph:
+        paragraph_separator = '\n\n'
+    return paragraph_separator.join(text)
 
 
 def run_tests():
-    print("\n\nTesting get_text_from_word_doc()\n")
+    print(f"\n\n{60*'_'}\nTesting get_text_from_word_doc()\n")
     print(get_text_from_word_doc('food_replicator.docx'))
+
+    print(f"\n\n{60*'_'}\nTesting get_text_from_word_doc()\n")
+    print(get_text_from_word_doc('food_replicator.docx', False))
 
 
 run_tests()
