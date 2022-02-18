441a9910e9a34dcb157e5ab4a88d216e28f0da49
Roanna Victorio <roannav@users.noreply.github.com>
2022-02-18 13:41:18 -0800

    print info (paragraphs, runs) about the document

--- get_text_from_word_doc.py
+++ get_text_from_word_doc.py
@@ -13,12 +13,34 @@ def get_text_from_word_doc( filename, double_space_between_paragraph=True):
     return paragraph_separator.join(text)
 
 
+def print_info_about_word_doc( filename):
+    doc = docx.Document(filename)
+    print(f"\n\n{60*'_'}\nInfo about {filename}")
+    num_paragraphs = len(doc.paragraphs)
+    print(f"Has {num_paragraphs} paragraphs")
+
+    for i in range(num_paragraphs):
+        print(f"\nParagraph #{i}")
+        print(doc.paragraphs[i].text)
+
+        num_runs = len(doc.paragraphs[i].runs)
+        print(f"\tHas {num_runs} runs")
+        # There is always 1+ runs per paragraph
+        for r in range(num_runs):
+            print(f"\n\tParagraph #{i},   run #{r}")
+            print(f"\t\thas style: {doc.paragraphs[i].runs[r].style}")
+            print(f"\t\t{doc.paragraphs[i].runs[r].text}")
+
+
+
 def run_tests():
     print(f"\n\n{60*'_'}\nTesting get_text_from_word_doc()\n")
     print(get_text_from_word_doc('food_replicator.docx'))
 
     print(f"\n\n{60*'_'}\nTesting get_text_from_word_doc()\n")
     print(get_text_from_word_doc('food_replicator.docx', False))
+
+    print_info_about_word_doc('food_replicator.docx')
 
 
 run_tests()
